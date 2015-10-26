1968e53e5abaa53ca609e389b8642f87aa4f73b0
Christian Nolte <github@die-familie-nolte.de>
2015-10-26 17:28:36 +0000

    cleanup

--- 1_pi_as_ap/do_it.sh
+++ 1_pi_as_ap/do_it.sh
@@ -12,24 +12,17 @@ sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-
 sudo iptables -t nat -S
 sudo iptables -S
-
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
-
 sudo wget http://adafruit-download.s3.amazonaws.com/adafruit_hostapd_14128.zip
 sudo unzip adafruit_hostapd_14128.zip
 sudo rm adafruit_hostapd_14128.zip
 sudo mv /usr/sbin/hostapd /usr/sbin/hostapd.ORIG
 sudo mv hostapd /usr/sbin
 sudo chmod 755 /usr/sbin/hostapd
-
-#sudo /usr/sbin/hostapd /etc/hostapd/hostapd.conf
-
 sudo service hostapd start 
 sudo service isc-dhcp-server start
-
 sudo service hostapd status
 sudo service isc-dhcp-server status
- 
+sudo reboot 
