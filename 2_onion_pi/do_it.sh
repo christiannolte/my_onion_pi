6a443bbccb4e0001a88c01f0ea9606e461658b4d
Christian Nolte <github@die-familie-nolte.de>
2015-10-28 08:52:10 +0000

    2nd. step the onion pi

---
+++ 2_onion_pi/do_it.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install tor
+sudo cp configs/torrc /etc/tor/torrc
+sudo service hostapd stop
+sudo cp configs/hostapd.conf /etc/hostapd/hostapd.conf
+sudo service hostapd start
+sudo service hostapd status
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22
+sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
+sudo iptables -t nat -L
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+sudo touch /var/log/tor/notices.log
+sudo chown debian-tor /var/log/tor/notices.log
+sudo chmod 644 /var/log/tor/notices.log
+ls -l /var/log/tor
+sudo service tor start
+sudo service tor status
+sudo update-rc.d tor enable
